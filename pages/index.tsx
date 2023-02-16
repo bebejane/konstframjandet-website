a47d8da030a07c6de9ce36188dbfc1c97cf2d1f2
Bebe Jane <bjornthief@gmail.com>
2023-02-16 18:36:41 +0100

    lissst

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,18 @@ import cn from 'classnames'
 import withGlobalProps from "/lib/withGlobalProps";
 import type { Menu } from "/lib/menu";
 import Link from "next/link";
+import { AllDistricsDocument } from '/graphql'
 
 export type Props = {
 	menu: Menu,
-	district: DistrictRecord
+	district: DistrictRecord,
+	districts: DistrictRecord[]
 }
 
-export default function Home({ menu, district }: Props) {
+export default function Home({ menu, district, districts }: Props) {
 	return (
 		<div className={s.container}>
-			District: {district?.name}
+			{district?.name}
 			<div className={s.color} style={{ backgroundColor: district?.color.hex }}></div>
 			<ul>
 				<li><Link href="/nyheter">Nyheter</Link></li>
@@ -20,12 +22,25 @@ export default function Home({ menu, district }: Props) {
 				<li><Link href="/projekt">Projekt</Link></li>
 				<li><Link href="/kontakt">Kontakt</Link></li>
 			</ul>
+			<br />
+			<h1>Alla distrikt</h1>
+			<ul>
+				{districts.map(({ subdomain, name }) =>
+					<li>
+						{subdomain ?
+							<Link href={`https://${subdomain}.konstframjandet.se`}>{name}</Link>
+							:
+							<>{name}</>
+						}
+					</li>
+				)}
+			</ul>
 		</div>
 	);
 }
 
 
-export const getStaticProps = withGlobalProps({ queries: [] }, async ({ props, revalidate, context }: any) => {
+export const getStaticProps = withGlobalProps({ queries: [AllDistricsDocument] }, async ({ props, revalidate, context }: any) => {
 
 	return {
 		props,
