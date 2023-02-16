05b6e43f03b3ef7881c45cf3358c9f4218ac33de
Bebe Jane <bjornthief@gmail.com>
2023-02-16 11:03:02 +0100

    test

--- pages/nyheter/[news].tsx
+++ pages/nyheter/[news].tsx
@@ -4,6 +4,7 @@ import { GetStaticProps } from "next";
 import { apiQuery } from "dato-nextjs-utils/api";
 import { NewsDocument, AllNewsDocument } from "/graphql";
 import { format } from "date-fns";
+import { apiQueryAll } from '/lib/utils';
 
 export type Props = {
   news: NewsRecord
@@ -18,8 +19,30 @@ export default function NewsItem({ news: { id, _createdAt, title, district, slug
   );
 }
 
-//NewsItem.page = { crumbs: [{ slug: 'nyheter', title: 'Nyheter' }], regional: true } as PageProps
+export async function getStaticPaths(context) {
+  const { news } = await apiQueryAll(AllNewsDocument)
+  const paths = news.map(({ slug }) => ({ params: { news: slug } }))
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
 
+
+export const getStaticProps = withGlobalProps({ queries: [] }, async ({ props, revalidate, context }: any) => {
+  const slug = context.params.news;
+  const { news } = await apiQuery(NewsDocument, { variables: { slug }, preview: context.preview })
+  return {
+    props: {
+      ...props,
+      news
+    },
+    revalidate
+  }
+})
+
+//NewsItem.page = { crumbs: [{ slug: 'nyheter', title: 'Nyheter' }], regional: true } as PageProps
+/*
 export const getServerSideProps = withGlobalProps({ queries: [] }, async ({ props, revalidate, context }: any) => {
 
   const slug = context.params.news;
@@ -40,3 +63,5 @@ export const getServerSideProps = withGlobalProps({ queries: [] }, async ({ prop
 export const config = {
   runtime: 'experimental-edge'
 }
+
+*/
