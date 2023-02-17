f0192f2b2ee418611c28e31e5111b464a98669e9
Bebe Jane <bjornthief@gmail.com>
2023-02-17 09:18:08 +0100

    clean

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -26,26 +26,18 @@ export default withRevalidate(async (record, revalidate) => {
       break;
   }
 
-  if (!revalidateSubdomain) {
-    console.log('revalidate subdomain req')
-    console.log({ ...payload, revalidateSubdomain: true })
+  if (revalidateSubdomain)
+    return revalidate(paths)
 
-    try {
-      await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
-        method: 'POST',
-        body: JSON.stringify({ entity: { ...payload, revalidateSubdomain: true } }),
-        headers: {
-          'Authorization': 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD),
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
-    } catch (err) {
-      console.error(err)
+  console.log(`revalidate subdomain: ${district.subdomain}`)
+  await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
+    method: 'POST',
+    body: JSON.stringify({ entity: { ...payload, revlidateSubdomain: true } }),
+    headers: {
+      'Authorization': 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD),
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
-    revalidate([])
-  } else {
-    console.log('revalidate district', district?.name, paths)
-    revalidate(paths)
-  }
+  })
+  //revalidate([])
 })
