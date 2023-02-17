cc6c98a52dece95383a825e4ea4c70459111cd9f
Bebe Jane <bjornthief@gmail.com>
2023-02-17 08:56:10 +0100

    test

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,6 +5,7 @@ export default withRevalidate(async (record, revalidate) => {
 
   const { api_key: apiKey, } = record.model;
   const { slug, _payload: payload } = record
+
   const paths = []
   const districtId = payload?.attributes?.district;
   const district = districts.find(el => el.id === districtId)
@@ -31,14 +32,14 @@ export default withRevalidate(async (record, revalidate) => {
     headers.set('Authorization', 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD));
     headers.set('Accept', 'application/json');
     headers.set('Content-Type', 'application/json');
-    console.log('https://${district.subdomain}.konstframjandet.se/api/revalidate')
-    /*
+    console.log(`https://${district.subdomain}.konstframjandet.se/api/revalidate`)
+
     await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
       method: 'POST',
       body: JSON.stringify({ ...payload, revalidateSubdomain: true }),
       headers
     })
-    */
+    revalidate([])
   } else {
     console.log('revalidate district', district?.name, paths)
     revalidate(paths)
