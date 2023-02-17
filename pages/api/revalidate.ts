b5f46484ffb09951c377ec3d58033bf1f696c116
Bebe Jane <bjornthief@gmail.com>
2023-02-17 09:02:04 +0100

    life

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -27,18 +27,18 @@ export default withRevalidate(async (record, revalidate) => {
 
   if (!revalidateSubdomain) {
     console.log('revalidate subdomain req')
+    console.log({ ...payload, revalidateSubdomain: true })
 
-    const headers = new Headers();
-    headers.set('Authorization', 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD));
-    headers.set('Accept', 'application/json');
-    headers.set('Content-Type', 'application/json');
-    console.log(`https://${district.subdomain}.konstframjandet.se/api/revalidate`)
-
-    await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
+    fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
       method: 'POST',
       body: JSON.stringify({ ...payload, revalidateSubdomain: true }),
-      headers
-    })
+      headers: {
+        'Authorization': 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD),
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => console.log('fetch done')).catch((err) => console.error(err))
+
     revalidate([])
   } else {
     console.log('revalidate district', district?.name, paths)
