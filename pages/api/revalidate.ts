0dbcedce3e4f3a781bd1eb615c7900b7e987c645
Bebe Jane <bjornthief@gmail.com>
2023-02-17 08:48:24 +0100

    test

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -24,19 +24,21 @@ export default withRevalidate(async (record, revalidate) => {
       break;
   }
 
-  if (false) {
+  if (!revalidateSubdomain) {
     console.log('revalidate subdomain req')
 
     const headers = new Headers();
     headers.set('Authorization', 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD));
     headers.set('Accept', 'application/json');
     headers.set('Content-Type', 'application/json');
-
+    console.log('https://${district.subdomain}.konstframjandet.se/api/revalidate')
+    /*
     await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
       method: 'POST',
       body: JSON.stringify({ ...payload, revalidateSubdomain: true }),
       headers
     })
+    */
   } else {
     console.log('revalidate district', district?.name, paths)
     revalidate(paths)
