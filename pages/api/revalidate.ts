7ab31cd8973eab81fa77388f73c3cc8ccae7d584
Bebe Jane <bjornthief@gmail.com>
2023-02-17 09:25:53 +0100

    ja

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -32,14 +32,18 @@ export default withRevalidate(async (record, revalidate) => {
     return console.log('no district found in payload')
 
   console.log(`revalidate subdomain: ${district.subdomain}`)
-  await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
-    method: 'POST',
-    body: JSON.stringify({ entity: { ...payload, revlidateSubdomain: true } }),
-    headers: {
-      'Authorization': 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD),
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
+  try {
+    await fetch(`https://${district.subdomain}.konstframjandet.se/api/revalidate`, {
+      method: 'POST',
+      body: JSON.stringify({ entity: { ...payload, revlidateSubdomain: true } }),
+      headers: {
+        'Authorization': 'Basic ' + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD),
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+  } catch (err) {
+    console.log('fetch error', err)
+  }
   revalidate([])
 })
