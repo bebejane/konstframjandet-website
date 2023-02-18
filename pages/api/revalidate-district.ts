10ff47ba1fe8d1aae4224b722144b82e436ee86a
Bebe Jane <bjornthief@gmail.com>
2023-02-18 13:41:04 +0100

    llslslslsl

--- pages/api/revalidate-district.ts
+++ pages/api/revalidate-district.ts
@@ -1,23 +1,25 @@
-import { withRevalidate } from 'dato-nextjs-utils/hoc'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { sleep } from '/lib/utils';
 import districts from '/lib/districts.json'
 
-export default withRevalidate(async (record, revalidate) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-  const { _payload: payload } = record
+  if (!basicAuth(req))
+    return res.status(401).send('Access denied')
 
+  const payload = req.body?.entity;
   const districtId = payload?.attributes?.district;
   const district = districts.find(el => el.id === districtId)
 
   if (!district)
-    return
+    return res.status(500).send('No district')
 
   const domain = `${district.subdomain}.konstframjandet.se`
   console.log(`revalidate subdomain: ${domain}`)
 
   fetch(`https://${domain}/api/revalidate`, {
     method: 'POST',
-    body: JSON.stringify({ entity: { ...payload } }),
+    body: JSON.stringify({ ...req.body }),
     headers: {
       'Authorization': 'Basic ' + Buffer.from(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD).toString('base64'),
       'Accept': 'application/json',
@@ -25,4 +27,18 @@ export default withRevalidate(async (record, revalidate) => {
     }
   }).then(() => console.log('sent req')).catch(err => console.error(err))
   await sleep(1000)
-})
+  return res.json({ revalidated: true })
+
+}
+
+export const basicAuth = (req: NextApiRequest) => {
+
+  const basicAuth = req.headers.authorization
+  if (!basicAuth)
+    return true;
+
+  const auth = basicAuth.split(' ')[1]
+  const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+  return user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD
+}
+
