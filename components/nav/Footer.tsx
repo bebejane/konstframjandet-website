a7333ed0fb82b4db616332cf23236c8743454738
Bebe Jane <bjornthief@gmail.com>
2023-03-09 15:31:11 +0100

    line footer

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -13,39 +13,41 @@ export default function Footer({ footer, districts }: FooterProps) {
 	return (
 		<footer className={cn(s.footer)} id="footer">
 			<div className={s.line}></div>
-			<div className={s.about}>
-				<p>Konstfrämjandet</p>
-				Konstfrämjandet är en organisation bildad 1947 som arbetar med konstbildning
-				och med att föra ut konst till människor i deras vardag. Vi verkar i hela Sverige.
-			</div>
-			<ul>
-				<li>
-					Aktuellt
-					<ul></ul>
-				</li>
-				<li>
-					Projekt
-					<ul></ul>
-				</li>
-				<li>
-					Distrikt
-					<ul>
-						{districts.map(({ id, subdomain, name }) =>
-							<li key={id}>
-								<Link href={`/`} locale={subdomain}>{name}</Link>
-							</li>
-						)}
-					</ul>
-				</li>
-				<li>
-					Om
-					<ul></ul>
-				</li>
-				<li>
-					Kontakt
-					<ul></ul>
-				</li>
-			</ul>
+			<nav>
+				<div className={s.about}>
+					<p>Konstfrämjandet</p>
+					Konstfrämjandet är en organisation bildad 1947 som arbetar med konstbildning
+					och med att föra ut konst till människor i deras vardag. Vi verkar i hela Sverige.
+				</div>
+				<ul>
+					<li>
+						Aktuellt
+						<ul></ul>
+					</li>
+					<li>
+						Projekt
+						<ul></ul>
+					</li>
+					<li>
+						Distrikt
+						<ul>
+							{districts.map(({ id, subdomain, name }) =>
+								<li key={id}>
+									<Link href={`/`} locale={subdomain}>{name}</Link>
+								</li>
+							)}
+						</ul>
+					</li>
+					<li>
+						Om
+						<ul></ul>
+					</li>
+					<li>
+						Kontakt
+						<ul></ul>
+					</li>
+				</ul>
+			</nav>
 		</footer>
 	)
 }
