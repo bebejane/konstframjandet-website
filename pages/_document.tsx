54eb4f94ffb1db48d571d21c735416b74b294b91
Bebe Jane <bjornthief@gmail.com>
2023-03-10 12:54:49 +0100

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { useRouter } from 'next/router';
-//import {}
 
 export default class MyDocument extends Document {
+
   render() {
-    const { district }: { district: DistrictRecord } = this.props?.__NEXT_DATA__?.props?.pageProps || {};
+    const { district: { color: { hex } } }: { district: DistrictRecord } = this.props?.__NEXT_DATA__?.props?.pageProps || {};
+    const isHome = this.props.dangerousAsPath === '/'
 
     return (
       <Html>
         <Head />
-        <body >
+        <body className={isHome ? 'home' : undefined}>
           <Main />
           <NextScript />
         </body>
