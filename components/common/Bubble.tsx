2ad00e03c0dd2144e906043ba193672a967df9a2
Bebe Jane <bjornthief@gmail.com>
2023-03-15 22:44:44 +0100

    ohh

--- components/common/Bubble.tsx
+++ components/common/Bubble.tsx
@@ -13,17 +13,23 @@ type Props = {
 export default function Bubble({ href, className, children }: Props) {
 
   const [styles, setStyles] = useState({})
+  const [toggle, setToggle] = useState(false)
 
   useEffect(() => {
     const corner = ['TopLeft', 'TopRight', 'BottomRight', 'BottomLeft'][randomInt(0, 3)]
-    setStyles({ [`border${corner}Radius`]: `${50 + randomInt(5, 10)}%` })
-  }, [])
+    setStyles({ [`border${corner}Radius`]: `${50 + randomInt(5, 8)}%` })
+  }, [toggle])
 
   const button = (
-    <button className={cn(s.bubble, 'mid', className)} style={styles}>
+    <button
+      className={cn(s.bubble, 'mid', className)}
+      style={styles}
+      onMouseLeave={() => setToggle(!toggle)}
+    >
       {children}
     </button>
   )
+
   return (
     href ? <Link href={href}>{button}</Link> : <>{button}</>
   )
