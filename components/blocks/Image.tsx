7b8cb6f716913ec1f8a5522bab36def7842b893f
Bebe Jane <bjornthief@gmail.com>
2023-03-27 12:00:58 +0200

    logotypes image block

--- components/blocks/Image.tsx
+++ components/blocks/Image.tsx
@@ -1,4 +1,5 @@
 import s from './Image.module.scss'
+import cn from 'classnames'
 import React from 'react'
 import { Image as DatoImage } from 'react-datocms'
 import { ImageGallery } from '/components'
@@ -6,15 +7,14 @@ import { DatoMarkdown as Markdown } from 'dato-nextjs-utils/components'
 
 export type ImageBlockProps = {
 	id: string,
-	data: ImageRecord,
+	data: ImageRecord
 	onClick: Function
 }
 
 export default function Image({ id, data: { image, layout }, onClick }: ImageBlockProps) {
 
 	return (
-
-		<figure className={s.single} onClick={() => onClick?.(image.id)}>
+		<figure className={cn(s.container, s[layout])} onClick={() => onClick?.(image.id)}>
 			<DatoImage
 				data={image.responsiveImage}
 				className={s.image}
--- components/blocks/StartText.tsx
+++ components/blocks/StartText.tsx
@@ -5,11 +5,7 @@ import Link from 'next/link'
 import { recordToSlug } from '/lib/utils'
 
 export type TextBlockProps = {
-<<<<<<< HEAD:components/blocks/Text.tsx
-  data: TextBlockRecord
-=======
   data: StartTextRecord
->>>>>>> 302d082999ac6561275e3e906fe1f01a12647ee5:components/blocks/StartText.tsx
 }
 
 export default function StartText({ data: { text, href } }: TextBlockProps) {
