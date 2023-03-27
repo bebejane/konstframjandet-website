b7c92834d09a5e519b6f3ef02dfcd599d6fe77d3
konst-teknik <information-konst-teknik.se>
2023-03-27 11:08:18 +0200

    Fix

---
+++ components/blocks/Text.tsx
@@ -0,0 +1,23 @@
+import s from './Text.module.scss'
+import cn from 'classnames'
+import React from 'react'
+import Link from 'next/link'
+import { recordToSlug } from '/lib/utils'
+
+export type TextBlockProps = {
+  data: ImageShortcutRecord
+}
+
+export default function Text({ data: { text } }: TextBlockProps) {
+
+  const href = link ? recordToSlug(link) : externalLink
+
+  return (
+    <section className={s.container}>
+      <Link href={href}>
+        {text}
+
+      </Link>
+    </section >
+  )
+}
