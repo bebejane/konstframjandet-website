6866d173ad97247b1453285b412de8e71292fa4f
Bebe Jane <bjornthief@gmail.com>
2023-03-27 11:27:31 +0200

    text block

--- components/blocks/StartText.tsx
+++ components/blocks/StartText.tsx
@@ -1,22 +1,19 @@
-import s from './Text.module.scss'
+import s from './StartText.module.scss'
 import cn from 'classnames'
 import React from 'react'
 import Link from 'next/link'
 import { recordToSlug } from '/lib/utils'
 
 export type TextBlockProps = {
-  data: ImageShortcutRecord
+  data: StartTextRecord
 }
 
-export default function Text({ data: { text } }: TextBlockProps) {
-
-  const href = link ? recordToSlug(link) : externalLink
+export default function StartText({ data: { text, href } }: TextBlockProps) {
 
   return (
     <section className={s.container}>
       <Link href={href}>
         {text}
-
       </Link>
     </section >
   )
