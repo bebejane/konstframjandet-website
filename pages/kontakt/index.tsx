da19e39d63fcd1527779268efc516c6e42c42014
konst-teknik <information-konst-teknik.se>
2023-04-04 13:34:12 +0200

    Newsletter

--- pages/kontakt/index.tsx
+++ pages/kontakt/index.tsx
@@ -20,14 +20,33 @@ export default function Contact({ district: { id, intro, contentContact }, distr
         content={contentContact}
       />
       <div className={s.newsletter}>
-        <h2>Nyhetsbrev</h2>
-        <p>
+        <p className="mid">Nyhetsbrev</p>
+        <p className="body-small">
           Anmäl dig till vårt nyhetsbrev för att få information om våra utställningar och projekt,
           och tips på andra konstrelaterade händelser.
         </p>
+
+        <form
+          action="//konstframjandet.us3.list-manage.com/subscribe/post?u=8b6aeb8402131fab41dc9e52c&amp;id=94772b4952"
+          method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+          target="_blank" novalidate>
+          <input type="email" value="" placeholder="Din epostadress" name="EMAIL" class="required email"
+            id="mce-EMAIL" />
+          <div id="mce-responses" class="clear">
+            <div id="mce-error-response" style="display:none"></div>
+            <div id="mce-success-response" style="display:none"></div>
+          </div>
+          <div style="position: absolute; left: -5000px;"><input type="text"
+            name="b_8b6aeb8402131fab41dc9e52c_94772b4952" tabindex="-1"
+            value="" /></div>
+          <input type="submit" value="Skicka" name="subscribe" id="mc-embedded-subscribe"
+            class="button" /></form>
+
+
+
         <input type="email" placeholder="E-post address..." />
-        <button>Skicka</button>
-      </div>
+        <button className="small">Skicka</button>
+      </div >
     </>
   );
 }
