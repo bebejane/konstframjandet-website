01d58c1283b3ac0245e4637017be7c018c6eebed
Bebe Jane <bjornthief@gmail.com>
2023-04-04 15:25:19 +0200

    NewsletterForm

---
+++ components/common/NewsletterForm.tsx
@@ -0,0 +1,78 @@
+import s from './NewsletterForm.module.scss'
+import Loader from '/components/common/Loader';
+import cn from 'classnames'
+
+import { useState } from 'react'
+import { isEmail } from "/lib/utils";
+
+export type Props = {
+
+}
+
+export default function NewsletterForm({ }: Props) {
+
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState<string | undefined>()
+  const [submitting, setSubmitting] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (!isEmail(email)) return setError('E-post adress ogiltig')
+
+    setSuccess(false)
+    setError(undefined)
+    setSubmitting(true)
+
+    setTimeout(() => setSubmitting(true), 0)
+    setTimeout(() => setSuccess(true), 2000)
+    setTimeout(() => setSubmitting(false), 2000)
+    return
+
+
+    fetch('/api/newsletter', {
+      body: JSON.stringify({ email }),
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+    }).then(async (res) => {
+      const { success, error } = await res.json()
+      setSuccess(success)
+      setError(error)
+    }).catch((err) => setError(err))
+      .finally(() => setSubmitting(false))
+  }
+
+  return (
+    <div className={s.newsletter}>
+      <p className="mid">Nyhetsbrev</p>
+      <p className="body-small">
+        Anmäl dig till vårt nyhetsbrev för att få information om våra utställningar och projekt,
+        och tips på andra konstrelaterade händelser.
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={({ target: { value } }) => setEmail(value)}
+          placeholder="E-post address..."
+          required
+          autoCapitalize="off"
+          autoCorrect="off"
+        />
+        {error && <div className={cn(s.error)}>{error}</div>}
+        <button className="small">Skicka</button>
+      </form>
+      {submitting && <div className={s.overlay}><Loader /></div>}
+      {success &&
+        <div className={cn(s.overlay, 'mid')}>
+          Tack för din anmälan!
+        </div>
+      }
+    </div >
+  )
+
+}
--- pages/kontakt/index.tsx
+++ pages/kontakt/index.tsx
@@ -1,7 +1,6 @@
-import s from "./index.module.scss";
 import withGlobalProps from "/lib/withGlobalProps";
 import { DistrictDocument } from "/graphql";
-import { Aside, Article } from "/components";
+import { Aside, Article, NewsletterForm } from "/components";
 
 export type Props = {
   district: DistrictRecord
@@ -19,34 +18,8 @@ export default function Contact({ district: { id, intro, contentContact }, distr
         intro={intro}
         content={contentContact}
       />
-      <div className={s.newsletter}>
-        <p className="mid">Nyhetsbrev</p>
-        <p className="body-small">
-          Anmäl dig till vårt nyhetsbrev för att få information om våra utställningar och projekt,
-          och tips på andra konstrelaterade händelser.
-        </p>
 
-        <form
-          action="//konstframjandet.us3.list-manage.com/subscribe/post?u=ç&amp;id=94772b4952"
-          method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-          target="_blank" novalidate>
-          <input type="email" value="" placeholder="Din epostadress" name="EMAIL" class="required email"
-            id="mce-EMAIL" />
-          <div id="mce-responses" class="clear">
-            <div id="mce-error-response" style="display:none"></div>
-            <div id="mce-success-response" style="display:none"></div>
-          </div>
-          <div style="position: absolute; left: -5000px;"><input type="text"
-            name="b_8b6aeb8402131fab41dc9e52c_94772b4952" tabindex="-1"
-            value="" /></div>
-          <input type="submit" value="Skicka" name="subscribe" id="mc-embedded-subscribe"
-            class="button" /></form>
-
-
-
-        <input type="email" placeholder="E-post address..." />
-        <button className="small">Skicka</button>
-      </div >
+      <NewsletterForm />
     </>
   );
 }
