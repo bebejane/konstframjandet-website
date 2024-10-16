c10b20f98303fa6efeacc2eafa19efc091304eba
konst-teknik <information@konst-teknik.se>
2024-10-16 11:30:36 +0200

    Remove sticky on aside if its taller than window height

--- components/layout/Aside.tsx
+++ components/layout/Aside.tsx
@@ -1,5 +1,5 @@
 import s from './Aside.module.scss'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import cn from 'classnames'
 import Link from 'next/link'
 import { useScrollInfo } from 'dato-nextjs-utils/hooks'
@@ -14,9 +14,35 @@ export type Props = {
 }
 
 export default function Aside({ children, title, titleHref, backLink, backLinkType, hideOnMobile = false }: Props) {
+  const [isTaller, setIsTaller] = useState(false);
+
+  useEffect(() => {
+    const checkHeight = () => {
+      const asideElement = document.querySelector("aside");
+      if (!asideElement) return;
+
+      const navElement = document.querySelector("nav");
+      const navHeight = navElement ? navElement.offsetHeight : 0;
+      const asideHeight = asideElement.offsetHeight;
+
+      if (asideHeight + navHeight > window.innerHeight) {
+        setIsTaller(true);
+      } else {
+        setIsTaller(false);
+        console.log(navHeight)
+      }
+    };
+
+    checkHeight();
+    window.addEventListener("resize", checkHeight);
+
+    return () => {
+      window.removeEventListener("resize", checkHeight);
+    };
+  }, []);
 
   return (
-    <aside className={cn(s.aside, hideOnMobile && s.hideMobile)}>
+    <aside className={cn(s.aside, hideOnMobile && s.hideMobile, isTaller && s.taller)}>
       {title && children ?
         titleHref ?
           <Link href={titleHref}><h3>{title}</h3></Link>
