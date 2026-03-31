aa1fc0be92ca4c974ec9522a69bddaa115bc192d
Bebe Jane <bjornthief@gmail.com>
2026-03-31 13:55:03 +0200

    proxy

--- proxy.ts
+++ proxy.ts
@@ -7,7 +7,8 @@ export default async function proxy(req: NextRequest) {
 	const pathname = req.nextUrl.pathname;
 	const domain = req.headers.get('host') as string;
 	const subdomain = districts.find((d) => d.subdomain === pathname.split('/')[1])?.subdomain;
-	const isAllowedDomain = domain === BASE_DOMAIN;
+	const isAllowedDomain = domain.endsWith(BASE_DOMAIN);
+	console.log({ domain, subdomain, isAllowedDomain });
 
 	if (!isAllowedDomain) return new Response(null, { status: 404 });
 
