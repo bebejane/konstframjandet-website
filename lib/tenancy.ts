dbaba57fba605587a0fbb66c0860a76821ae4e05
Bebe Jane <bjornthief@gmail.com>
2026-03-31 16:55:54 +0200

    ahhhh

--- lib/tenancy.ts
+++ lib/tenancy.ts
@@ -10,7 +10,7 @@ export function getTenantUrl(subdomain?: string, pathname = '/') {
 		subdomain === PRIMARY_SUBDOMAIN ? (prod ? 'www.' : '') : prod ? subdomain : `/${subdomain}`;
 
 	return prod
-		? `${BASE_PROTOCOL}${sub}.${BASE_DOMAIN}${pathname}`
+		? `${BASE_PROTOCOL}${sub}${BASE_DOMAIN}${pathname}`
 		: `${BASE_PROTOCOL}${BASE_DOMAIN}${sub}${pathname}`;
 }
 
